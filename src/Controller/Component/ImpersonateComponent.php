c9f4252ddd9c34134279b49ecfd33373465e0712
Chris Hallgren <challgren@gmail.com>
2019-01-03 17:39:53 -0600

    Changing to MIT license

--- src/Controller/Component/ImpersonateComponent.php
+++ src/Controller/Component/ImpersonateComponent.php
@@ -1,12 +1,16 @@
 <?php
 /**
- * Impersonate component for Cakephp3
+ * CakeImpersonate : Impersonate Plugin
+ * Copyright (c) jomweb
  *
- * Component to use impersonate to easily access as others user account
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
  *
- *
+ * @copyright     Copyright (c) jomweb
+ * @since         3.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
-
 namespace CakeImpersonate\Controller\Component;
 
 use Cake\Controller\Component;
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,17 @@
 <?php
 /**
+ * CakeImpersonate : Impersonate Plugin
+ * Copyright (c) jomweb
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) jomweb
+ * @since         3.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+/**
  * Test suite bootstrap for Cakeimpersonate.
  *
  * This function is used to find the location of CakePHP whether CakePHP
